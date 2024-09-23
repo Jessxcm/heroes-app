ee6cd5b952dfbdda5f69a6f52a5f8aae262d434c
Jessica <calajess@gmail.com>
2024-09-23 16:08:36 -0600

    adding auth

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,10 +1,18 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context";
 
 export const LoginPage = () => {
+
+  const {login} = useContext(AuthContext);
+
+
   const navigate = useNavigate();
   
   const onLogin = () =>{
-    
+  
+    login('Jessica Cala');
+
     navigate('/', {
       replace: true,
     });
